39733f86aa3c72d097ed42441f84cb84ab6f8314
mohanchalla <mohanchalla10@gmail.com>
2018-11-07 23:39:27 +0530

    Postfix Evalution

---
+++ src/main/java/problems/PostfixEvaluation.java
@@ -0,0 +1,71 @@
+package problems;
+
+import com.datastructors.Stack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PostfixEvaluation {
+    private static final List<Character> operators;
+
+    static {
+        operators = new ArrayList<>();
+        operators.add('+');
+        operators.add('-');
+        operators.add('*');
+        operators.add('/');
+    }
+
+    public static void main(String[] args) {
+        String exp = "2 3 * 5 4 * + 9 -";
+        int result = evaluatePostfixExpression(exp);
+        System.out.println(result);
+    }
+
+    private static int evaluatePostfixExpression(String exp) {
+        Stack<Integer> stack = new Stack<>();
+        String[] tokens = exp.split(" ");
+        for (int i = 0; i < tokens.length; i++) {
+            char token = tokens[i].charAt(0);
+            if (isOperand(token)) {
+                stack.push(Character.getNumericValue(token));
+            } else {
+                int op2 = stack.pop();
+                int op1 = stack.pop();
+                int res = perform(op1, op2, token);
+                stack.push(res);
+            }
+        }
+        return stack.pop();
+    }
+
+    private static int perform(int op1, int op2, char token) {
+        int result;
+        switch (token) {
+            case '+':
+                result = op1 + op2;
+                break;
+            case '-':
+                result = op1 - op2;
+                break;
+            case '*':
+                result = op1 * op2;
+                break;
+            case '/':
+                result = op1 / op2;
+                break;
+            default:
+                throw new RuntimeException("Invalid operator!");
+
+        }
+        return result;
+    }
+
+    private static boolean isOperator(Character token) {
+        return operators.contains(token);
+    }
+
+    private static boolean isOperand(Character token) {
+        return !isOperator(token);
+    }
+}
