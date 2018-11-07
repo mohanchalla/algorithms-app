f260068ae466c08c683268c731c39a23d8b0a885
mohanchalla <mohanchalla10@gmail.com>
2018-11-07 16:25:24 +0530

    Create Queue Using Stack Data structor

---
+++ src/main/java/problems/QueueInOneStack.java
@@ -0,0 +1,41 @@
+package problems;
+
+import com.datastructors.Stack;
+import com.exception.QueueEmptyException;
+
+public class QueueInOneStack<E extends Comparable<E>> {
+    Stack<E> stack = new Stack<>();
+
+    public void enqueue(E data) {
+        stack.push(data);
+    }
+
+    public E dequeue() {
+        if (stack.isEmpty())
+            throw new QueueEmptyException();
+        if (stack.size() == 1)
+            return stack.pop();
+        else {
+            E x =  stack.pop();
+            E res = dequeue();
+            stack.push(x);
+            return res;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return stack.toString();
+    }
+
+    public static void main(String[] args) {
+        QueueInOneStack<Integer> queue = new QueueInOneStack<>();
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.enqueue(4);
+        System.out.println(queue.dequeue());
+        queue.enqueue(5);
+        System.out.println(queue);
+    }
+}
---
+++ src/main/java/problems/QueueInTwoStacks.java
@@ -0,0 +1,42 @@
+package problems;
+
+import com.datastructors.Stack;
+import com.exception.QueueEmptyException;
+
+
+/**
+ * Algorithm:
+ *
+ */
+public class QueueInTwoStacks<E extends Comparable<E>> {
+    private Stack<E> stack1 = new Stack<>();
+    private Stack<E> stack2 = new Stack<>();
+
+    public void enqueue(E data) {
+        stack1.push(data);
+    }
+
+    public E peek() {
+        return stack2.peek();
+    }
+
+    public E dequeue() {
+        if (stack1.isEmpty() && stack2.isEmpty())
+            throw new QueueEmptyException();
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+
+    public static void main(String[] args) {
+        QueueInTwoStacks<Integer> stacks = new QueueInTwoStacks<>();
+        stacks.enqueue(1);
+        stacks.enqueue(2);
+        stacks.enqueue(3);
+        stacks.enqueue(4);
+        System.out.println(stacks.dequeue());
+    }
+}
